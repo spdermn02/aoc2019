76d15d8b7a9f021002f93e85b072cd42d37fa1cd
Jameson Allen <jameson.f.allen@gmail.com>
2019-12-06 14:25:17 -0600

    Completed Day 4 both parts

--- Day 4/p1.pl
+++ Day 4/p1.pl
@@ -1,5 +1,35 @@
 
 use feature 'say';
 
-my $i = <<'EOF';
-EOF
+my $start = 387638;
+my $end = 919123;
+
+my %valid = ();
+my $count = 0;
+for( my $i = $start; $i < $end; $i++ ){
+    my $invalid = 0;
+    if( $i =~ /([0-9])\1+/ ) {
+        my @nums = split(//,$i);
+        my $prev = shift @nums;
+        foreach my $n ( @nums ) {
+            if( $n < $prev)
+            {
+                $invalid = 1;
+            }
+            $prev = $n;
+        }
+        if( $invalid ) {
+            next;
+        }
+        else {
+            $valid{$i} = 1;
+            $count++;
+        }
+    }
+    else {
+        next;
+    }
+}
+say $count;
+#use Data::Dumper;
+#say Dumper(\%valid);
--- Day 4/p2.pl
+++ Day 4/p2.pl
@@ -1,4 +1,36 @@
 use feature 'say';
 
-my $i = <<'EOF';
-EOF
+my $start = 387638;
+my $end = 919123;
+
+my %valid = ();
+my $count = 0;
+for( my $i = $start; $i < $end; $i++ ){
+    my $invalid = 0;
+    my %numCounts = ();
+    if( $i =~ /(?:^|(.)(?!\1))(\d)\2(?!\2)/) {
+        my @nums = split(//,$i);
+        my $prev = shift @nums;
+        foreach my $n ( @nums ) {
+            if( $n < $prev)
+            {
+                $invalid = 1;
+            }
+            $prev = $n;
+            $numCounts{$n}++;
+        }
+        if( $invalid ) {
+            next;
+        }
+        else {
+            $valid{$i} = 1;
+            $count++;
+        }
+    }
+    else {
+        next;
+    }
+}
+say $count;
+#use Data::Dumper;
+#say Dumper(\%valid);
